a0210461fe23add5f09d3c9d1d1d5051c89aad00
x383494 <vq.66@qq.com>
2024-03-10 10:09:23 +0800

    merge master

--- src/ast_defs.hpp
+++ src/ast_defs.hpp
@@ -2,15 +2,61 @@
 #include <iostream>
 #include <memory>
 
+constexpr const char* INDENT = "  ";
+
 class BaseAST{
 public:
-	~BaseAST() = default;
-	friend std::ostream &operator<<(std::ostream &cout, const BaseAST &me){
-		return cout;
+	virtual ~BaseAST() = default;
+	virtual void output(std::string) const = 0;
+};
+
+class CompUnitAST: public BaseAST{
+public:
+	std::unique_ptr<BaseAST> func_def;
+	void output(std::string prefix) const override{
+		std::cout << prefix << "CompUnit{\n";
+		func_def->output(prefix + INDENT);
+		std::cout << '\n' << prefix << "}";
 	}
 };
 
-class CompUnitAST{
+class FuncDefAST: public BaseAST{
 public:
-	std::shared_ptr<BaseAST> func_def;
+	std::unique_ptr<BaseAST> func_typ;
+	std::string ident;
+	std::unique_ptr<BaseAST> block;
+	void output(std::string prefix) const override{
+		std::cout << prefix << "FuncDef(";
+		func_typ->output("");
+		std::cout << ident << "){\n";
+		block->output(prefix + INDENT);
+		std::cout << "\n" << prefix << "}";
+	}
+};
+
+
+class TypAST: public BaseAST{
+public:
+	std::string typ;
+	void output(std::string) const override{
+		std::cout << typ;
+	}
+};
+
+class BlockAST: public BaseAST{
+public:
+	std::unique_ptr<BaseAST> stmt;
+	void output(std::string prefix) const override{
+		std::cout << prefix << "BlkAST{\n";
+		stmt->output(prefix + INDENT);
+		std::cout << '\n' << prefix << "}";
+	}
+};
+
+class StmtAST: public BaseAST{
+public:
+	int ret_val;
+	void output(std::string prefix) const override{
+		std::cout << "RET " << ret_val;
+	}
 };
