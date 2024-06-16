1ef164c66c27c955999bb8449dde5e91d7d2d383
x383494 <vq.66@qq.com>
2024-06-16 20:39:09 +0800

    lv 4.2

--- src/ir.cpp
+++ src/ir.cpp
@@ -1,33 +1,89 @@
 #include <cassert>
 #include <iostream>
+#include <memory>
+#include <stack>
 #include <unordered_map>
 
 #include "ir.hpp"
 #include "koopa.h"
 
-// class Asm_val{
-// private:
-// 	int val;
-// 	bool is_im;
-// public:
-// 	Asm_val(bool typ, int x){
-// 		is_im = typ;
-// 		val = x;
-// 	}
-// 	template<typename T>
-// 	friend T &operator<<(T& outstr, Asm_val &me){
-// 		if(me.is_im){
-// 			outstr << me.val;
-// 		} else {
-// 			outstr << "a" << me.val;
-// 		}
-// 		return outstr;
-// 	}
-// };
+class Asm_val {
+	int data;
 
-int var_cnt = 0;
+public:
+	bool assigned;
+	bool is_im;
 
-std::unordered_map<void *, int> valmp;
+	Asm_val(int dat, bool isim) {
+		data = dat;   // input_offset or immediate_number
+		is_im = isim;
+		assigned = isim;
+	}
+	template<typename T>
+	void assign_from_t0(T &outstr) {
+		assert(!is_im);
+		outstr << "sw t0, " << data << "(sp)\n";
+		assigned = true;
+	}
+	template<typename T>
+	void load(T &outstr, std::string reg) {
+		if(is_im) {
+			outstr << "li " << reg << ", " << data << '\n';
+		} else {
+			outstr << "lw " << reg << ", " << data << "(sp)\n";
+		}
+	}
+};
+
+namespace Global_State {
+
+int offset_cnt;
+std::stack<int> function_stack_mem;
+
+}   // namespace Global_State
+
+// koopa_raw_
+std::unordered_map<void *, std::shared_ptr<Asm_val>> valmp;
+
+// return mem(byte).
+
+int get_function_stack_mem(const koopa_raw_value_t &val) {
+	if(val->ty->tag == KOOPA_RTT_UNIT) {
+		return 0;
+	}
+	valmp[(void *)val] = std::make_shared<Asm_val>(Global_State::offset_cnt, false);
+	Global_State::offset_cnt += 4;
+	return 4;
+	// const auto &kind = val->kind;
+	// switch(kind.tag) {
+	// case KOOPA_RVT_BINARY:
+	// case KOOPA_RVT_INTEGER:
+	//	// ...
+	// default:
+	//	return 0;
+	// }
+}
+
+int get_function_stack_mem(const koopa_raw_basic_block_t &blk) {
+	int sum_size = 0;
+	for(size_t i = 0; i < blk->insts.len; i++) {
+		assert(blk->insts.kind == KOOPA_RSIK_VALUE);
+		koopa_raw_value_t val = (koopa_raw_value_t)blk->insts.buffer[i];
+		sum_size += get_function_stack_mem(val);
+	}
+	return sum_size;
+}
+
+int get_function_stack_mem(const koopa_raw_function_t &func) {
+	int sum_size = 0;
+	Global_State::offset_cnt = 0;
+	for(size_t i = 0; i < func->bbs.len; i++) {
+		assert(func->bbs.kind == KOOPA_RSIK_BASIC_BLOCK);
+		koopa_raw_basic_block_t blk = (koopa_raw_basic_block_t)func->bbs.buffer[i];
+		sum_size += get_function_stack_mem(blk);
+	}
+	return sum_size;
+}
 
 void dfs_ir(const koopa_raw_program_t &prog, Outp &outstr) {
 	outstr << ".text\n";
@@ -41,11 +97,16 @@ void dfs_ir(const koopa_raw_program_t &prog, Outp &outstr) {
 
 void dfs_ir(const koopa_raw_function_t &func, Outp &outstr) {
 	outstr << (func->name + 1) << ":\n";
+	int mem = get_function_stack_mem(func);
+	Global_State::function_stack_mem.push(mem);
+	outstr << "li t0, " << -mem << "\nadd sp, sp, t0\n";
 	for(size_t i = 0; i < func->bbs.len; i++) {
 		assert(func->bbs.kind == KOOPA_RSIK_BASIC_BLOCK);
 		koopa_raw_basic_block_t blk = (koopa_raw_basic_block_t)func->bbs.buffer[i];
 		dfs_ir(blk, outstr);
 	}
+	Global_State::function_stack_mem.pop();
+	// ret: sp -= mem;
 }
 void dfs_ir(const koopa_raw_basic_block_t &blk, Outp &outstr) {
 	for(size_t i = 0; i < blk->insts.len; i++) {
@@ -56,20 +117,36 @@ void dfs_ir(const koopa_raw_basic_block_t &blk, Outp &outstr) {
 }
 
 void dfs_ir(const koopa_raw_value_t &val, Outp &outstr) {
-	if(valmp.contains((void *)val)) return;
+	if(val->ty->tag != KOOPA_RTT_UNIT && val->kind.tag != KOOPA_RVT_INTEGER) {
+		assert(valmp.contains((void *)val));
+		if(valmp[(void *)val]->assigned) return;
+	}
 	const auto &kind = val->kind;
 	switch(kind.tag) {
 	case KOOPA_RVT_BINARY:
+		if(!valmp.contains((void *)&kind.data.binary)) {
+			valmp[(void *)&kind.data.binary] = valmp[(void *)val];
+		}
 		dfs_ir(kind.data.binary, outstr);
-		valmp[(void *)val] = valmp[(void *)&kind.data.binary];
 		break;
 	case KOOPA_RVT_RETURN:
+		valmp[(void *)&kind.data.ret] = valmp[(void *)val];
 		dfs_ir(kind.data.ret, outstr);
 		break;
 	case KOOPA_RVT_INTEGER:
-		outstr << "li t" << var_cnt << ", " << kind.data.integer.value << '\n';
-		valmp[(void *)val] = var_cnt;
-		var_cnt++;
+		if(valmp.contains((void *)val)) break;
+		valmp[(void *)val] = std::make_shared<Asm_val>(kind.data.integer.value, true);
+		break;
+	case KOOPA_RVT_ALLOC:
+		break;
+	case KOOPA_RVT_STORE:
+		dfs_ir(kind.data.store.value, outstr);
+		valmp[(void *)kind.data.store.value]->load(outstr, "t0");
+		valmp[(void *)kind.data.store.dest]->assign_from_t0(outstr);
+		break;
+	case KOOPA_RVT_LOAD:
+		valmp[(void *)kind.data.load.src]->load(outstr, "t0");
+		valmp[(void *)val]->assign_from_t0(outstr);
 		break;
 	default:
 		assert(0);
@@ -77,16 +154,24 @@ void dfs_ir(const koopa_raw_value_t &val, Outp &outstr) {
 }
 
 void dfs_ir(const koopa_raw_return_t &ret, Outp &outstr) {
-	dfs_ir(ret.value, outstr);
-	outstr << "mv a0, t" << valmp[(void *)ret.value] << "\n";
-	outstr << "ret\n";
+	if(!valmp.contains((void *)ret.value)) {
+		valmp[(void *)ret.value] = valmp[(void *)&ret];
+		dfs_ir(ret.value, outstr);
+	}
+	valmp[(void *)ret.value]->load(outstr, "a0");
+	outstr << "li t0, " << Global_State::function_stack_mem.top() << "\n"
+		   << "add sp, sp, t0\n"
+		   << "ret\n";
 	return;
 }
 
 void dfs_ir(const koopa_raw_binary_t &bin, Outp &outstr) {
-	if(valmp.contains((void *)&bin)) return;
+	assert(valmp.contains((void *)&bin));
+	if(valmp[(void *)&bin]->assigned) return;
 	dfs_ir(bin.lhs, outstr);
 	dfs_ir(bin.rhs, outstr);
+	valmp[(void *)bin.lhs]->load(outstr, "t0");
+	valmp[(void *)bin.rhs]->load(outstr, "t1");
 	switch(bin.op) {
 	case KOOPA_RBO_ADD:
 		outstr << "add";
@@ -125,24 +210,18 @@ void dfs_ir(const koopa_raw_binary_t &bin, Outp &outstr) {
 		std::cerr << "Unsupported binary operator: " << bin.op << '\n';
 		assert(0);
 	}
-	outstr << " t" << var_cnt
-		   << ", "
-		   << "t" << valmp[(void *)bin.lhs]
-		   << ", "
-		   << "t" << valmp[(void *)bin.rhs]
-		   << '\n';
+	outstr << " t0, t0, t1\n";
 	switch(bin.op) {
 	case KOOPA_RBO_EQ:
 	case KOOPA_RBO_LE:
 	case KOOPA_RBO_GE:
-		outstr << "seqz t" << var_cnt << ", t" << var_cnt << '\n';
+		outstr << "seqz t0, t0\n";
 		break;
 	case KOOPA_RBO_NOT_EQ:
-		outstr << "snez t" << var_cnt << ", t" << var_cnt << '\n';
+		outstr << "snez t0, t0\n";
 		break;
 	default:
 		break;
 	}
-	valmp[(void *)&bin] = var_cnt;
-	var_cnt++;
+	valmp[(void *)&bin]->assign_from_t0(outstr);
 }
